640d9391c3e102f12241810f1005ea245f40631a
offiahchiaz <offiahchiaz@gmail.com>
2021-02-11 23:34:11 +0100

    Updated models with their respective relationships

--- app/Question.php
+++ app/Question.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function questionnnaire()
+    {
+        return $this->belongsTo(Questionnnaire::class);
+    }
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+    }
 }
